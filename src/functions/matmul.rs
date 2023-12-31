4444dfd6183f4a2f259b6e9a9b424f1efeb46a16
Ryo-sunken <bellman.toratora@gmail.com>
2024-01-01 04:03:20 +0900

    行列積の実装

--- src/functions/matmul.rs
+++ src/functions/matmul.rs
@@ -1,5 +1,8 @@
 use crate::core::handle::{Function, VariableHandle};
-use std::ops::{Div, Mul};
+use std::{
+    iter::Sum,
+    ops::{Div, Mul},
+};
 
 impl<'a, T> VariableHandle<'a, T>
 where
@@ -32,5 +35,24 @@ where
         self.tape
             .variable(result)
             .set_creator(Function::CWiseDiv(self.data_idx, rhs.data_idx))
+    }
+}
+
+impl<'a, T> Mul for VariableHandle<'a, T>
+where
+    T: Sum + Mul<Output = T> + Copy,
+{
+    type Output = VariableHandle<'a, T>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let result = {
+            let binding = self.tape.nodes.borrow();
+            let var_self = binding.get(self.data_idx).unwrap();
+            let var_rhs = binding.get(rhs.data_idx).unwrap();
+            var_self * var_rhs
+        };
+        self.tape
+            .variable(result)
+            .set_creator(Function::MatMul(self.data_idx, rhs.data_idx))
     }
 }
