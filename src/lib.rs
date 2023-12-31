4bc28bafd0f797d76fbf3628efc729d7181015d5
Ryo-sunken <bellman.toratora@gmail.com>
2024-01-01 03:53:58 +0900

    テストの追加

--- src/lib.rs
+++ src/lib.rs
@@ -25,4 +25,24 @@ mod tests {
         assert_eq!(z.data(), Matrix::new([[3., 3., 3.]]));
         assert_eq!(z.creator.unwrap(), Function::Sub(x.data_idx, y.data_idx));
     }
+
+    #[test]
+    fn cwise_mul() {
+        let tape = VariableTape::new();
+        let x = tape.variable(Matrix::new([[1.,2.,3.]]));
+        let y = tape.variable(Matrix::new([[2.,3.,4.]]));
+        let z = x.cwise_mul(y);
+        assert_eq!(z.data(), Matrix::new([[2.,6.,12.]]));
+        assert_eq!(z.creator.unwrap(), Function::CWiseMul(x.data_idx, y.data_idx));
+    }
+
+    #[test]
+    fn cwise_div() {
+        let tape = VariableTape::new();
+        let x = tape.variable(Matrix::new([[6.,9.,10.]]));
+        let y = tape.variable(Matrix::new([[2.,3.,5.]]));
+        let z = x.cwise_div(y);
+        assert_eq!(z.data(), Matrix::new([[3.,3.,2.]]));
+        assert_eq!(z.creator.unwrap(), Function::CWiseDiv(x.data_idx, y.data_idx));
+    }
 }
