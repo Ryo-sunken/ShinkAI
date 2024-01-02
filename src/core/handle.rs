b7596d5d7e3e80dedeedf567114e90f5d399b6ee
Ryo-sunken <bellman.toratora@gmail.com>
2024-01-02 17:27:14 +0900

    variableの設計変更

--- src/core/handle.rs
+++ src/core/handle.rs
@@ -2,53 +2,15 @@ use crate::core::tape::VariableTape;
 use matrix::matrix::{Axis, Matrix};
 
 #[allow(dead_code)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub(crate) enum Function<T> {
-    Add(usize, usize),
-    Sub(usize, usize),
-    MatMul(usize, usize),
-    Neg(usize),
-    ScalarMul(usize, T),
-    ScalarDiv(usize, T),
-    CWiseMul(usize, usize),
-    CWiseDiv(usize, usize),
-    Transpose(usize),
-    Pow(usize, T),
-    Exp(usize),
-    Sin(usize),
-    Cos(usize),
-    Sqrt(usize),
-    Min(usize, Option<Axis>),
-    Max(usize, Option<Axis>),
-    Sum(usize, Option<Axis>),
-}
-
-#[allow(dead_code)]
 #[derive(Clone, Copy, Debug)]
 pub struct VariableHandle<'a, T> {
     pub(crate) tape: &'a VariableTape<T>,
     pub(crate) data_idx: usize,
-    pub(crate) grad_idx: Option<usize>,
-    pub(crate) creator: Option<Function<T>>,
 }
 
 impl<'a, T> VariableHandle<'a, T> {
     pub(crate) fn new(tape: &'a VariableTape<T>, data_idx: usize) -> Self {
-        Self {
-            tape,
-            data_idx,
-            grad_idx: None,
-            creator: None,
-        }
-    }
-
-    pub(crate) fn set_creator(self, creator: Function<T>) -> Self {
-        Self {
-            tape: self.tape,
-            data_idx: self.data_idx,
-            grad_idx: self.grad_idx,
-            creator: Some(creator),
-        }
+        Self { tape, data_idx }
     }
 }
 
--- src/core/tape.rs
+++ src/core/tape.rs
@@ -1,11 +1,49 @@
 use crate::core::handle::VariableHandle;
 use matrix::matrix::Matrix;
-use std::cell::RefCell;
+use std::{arch::x86_64, cell::RefCell};
+
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub(crate) enum Function<T> {
+    Add(usize, usize),
+    Sub(usize, usize),
+    MatMul(usize, usize),
+    Neg(usize),
+    ScalarMul(usize, T),
+    ScalarDiv(usize, T),
+    CWiseMul(usize, usize),
+    CWiseDiv(usize, usize),
+    Transpose(usize),
+    Pow(usize, T),
+    Exp(usize),
+    Sin(usize),
+    Cos(usize),
+    Sqrt(usize),
+    Min(usize, Option<Axis>),
+    Max(usize, Option<Axis>),
+    Sum(usize, Option<Axis>),
+}
+
+#[allow(dead_code)]
+pub struct Variable<T> {
+    pub(crate) data: Matrix<T>,
+    pub(crate) grad: Option<Matrix<T>>,
+    pub(crate) creator: Option<Function<T>>,
+    pub(crate) generation: usize,
+}
 
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub struct VariableTape<T> {
-    pub(crate) nodes: RefCell<Vec<Matrix<T>>>,
+    pub(crate) nodes: RefCell<Vec<Variable<T>>>,
+}
+
+fn max(x: usize, y: usize) -> usize {
+    if x > y {
+        x
+    } else {
+        y
+    }
 }
 
 impl<T> VariableTape<T> {
@@ -16,7 +54,28 @@ impl<T> VariableTape<T> {
     }
 
     pub fn variable(&self, data: Matrix<T>) -> VariableHandle<T> {
-        self.nodes.borrow_mut().push(data);
+        self.nodes.borrow_mut().push(Variable {
+            data,
+            grad: None,
+            creator: None,
+            generation: 0,
+        });
         VariableHandle::<T>::new(self, self.nodes.borrow().len() - 1)
+    }
+
+    pub fn from_function(&self, data: Matrix<T>, creator: Function<T>) -> VariableHandle<T> {
+        let generation = self.generation(creator);
+        self.nodes.borrow_mut().push(Variable {
+            data,
+            grad: None,
+            creator: Some(creator),
+            generation,
+        });
+        VariableHandle::<T>::new(self, self.nodes.borrow().len() - 1)
+    }
+
+    fn generation(&self, creator: Function<T>) -> usize {
+        // TODO: Functionごとのgenerationの計算
+        1
     }
 }
